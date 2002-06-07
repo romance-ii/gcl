efa4944f1e386a77b6dede5dba44d80758e32bf3
Mike Thomas <mike.thomas@paradigmgeo.com>
2002-06-07 04:03:54 +0000

    Allow a return to the parent process after launching the server.

--- gcl/gcl-tk/gcltksrv.bat
+++ gcl/gcl-tk/gcltksrv.bat
@@ -1,7 +1,7 @@
 set GCL_TK_DIR=c:/cvs/gcl/gcl-tk
 set TCL_LIBRARY=c:/lang/tcl/lib/tcl8.3
 set TK_LIBRARY=c:/lang/tcl/lib/tcl8.3
-%GCL_TK_DIR%/gcltkaux %1 %2 %3
+start %GCL_TK_DIR%/gcltkaux %1 %2 %3
 
 
 
