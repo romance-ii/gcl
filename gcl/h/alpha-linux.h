857c4228949ab6c0931dc3d754af65e40822cca5
Camm Maguire <camm@debian.org>
2002-08-02 18:38:04 +0000

    sgc enabled for alpha-linux

--- gcl/h/alpha-linux.h
+++ gcl/h/alpha-linux.h
@@ -19,3 +19,12 @@
 
 #define MUST_COPY_VA_LIST
 #define NULL_OR_ON_C_STACK(x) ((x)==0 || ((((unsigned long)x) > 0x100000000) && ((unsigned long)x) < 0x120000000))
+
+#undef MPROTECT_ACTION_FLAGS
+#define MPROTECT_ACTION_FLAGS SA_RESTART|SA_SIGINFO
+#ifdef IN_GBC
+#include <ucontext.h>
+#define GET_FAULT_ADDR(sig,code,scp,addr) \
+  (char *)((struct ucontext *)scp )->uc_mcontext.sc_traparg_a0
+#endif
+#define SGC
