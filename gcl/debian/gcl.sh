a052a36b3990e714bb95c774ac8be3a7fa274b69
Camm Maguire <camm@debian.org>
2003-02-27 01:07:34 +0000

    ANSI toggling fixes for Debian package

--- gcl/debian/gcl.sh
+++ gcl/debian/gcl.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 export C_INCLUDE_PATH=/fix/s/camm/gcl/h:$C_INCLUDE_PATH
 if [ "$GCL_ANSI" != "" ] ; then
-exec /fix/s/camm/gcl/unixport/saved_ansi_gcl \
-   -dir /fix/s/camm/gcl/unixport/ \
-   -libdir /fix/s/camm/gcl/ \
+exec gcl/unixport/saved_ansi_gcl \
+   -dir gcl/unixport/ \
+   -libdir gcl/ \
    -eval '(setq si::*allow-gzipped-file* t)' \
    -eval '(setq si::*tk-library* "/usr/lib/tk8.3/../tk8.3")' \
    -eval '(si::init-readline)' \
      "$@"
 else
-exec /fix/s/camm/gcl/unixport/saved_gcl \
-   -dir /fix/s/camm/gcl/unixport/ \
-   -libdir /fix/s/camm/gcl/ \
+exec gcl/unixport/saved_gcl \
+   -dir gcl/unixport/ \
+   -libdir gcl/ \
    -eval '(setq si::*allow-gzipped-file* t)' \
    -eval '(setq si::*tk-library* "/usr/lib/tk8.3/../tk8.3")' \
    -eval '(si::init-readline)' \
