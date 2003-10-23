2f181695e2c4cdd55e2520e5d0620ee1b80dd673
Camm Maguire <camm@debian.org>
2003-10-23 15:28:14 +0000

    Add clcs_macros and clcs package info to image before pcl build, as walker needs final forms of certain macros/fuctions to generate correct generic function calls, and clcs replaces/shadows some semi-equivalent macros/functions in the traditional image, e.g. etypecase, etc.

---
+++ gcl/clcs/myload1.lisp
@@ -0,0 +1,40 @@
+(load "../clcs/clcs_macros.lisp")
+
+(in-package "CONDITIONS")
+
+(defvar *shadowed-symbols1* 
+  '(ASSERT ETYPECASE CTYPECASE ECASE CCASE))
+
+(defun install-symbol (real clcs)
+  (unless (get real 'definition-before-clcs)
+    (setf (get real 'definition-before-clcs)
+	  (symbol-function real)))
+  (unless (eq (symbol-function real)
+	      (symbol-function clcs))	       
+    (setf (symbol-function real)
+	  (symbol-function clcs))))
+
+(defun revert-symbol (real)
+  (when (and (get real 'definition-before-clcs)
+	     (not (eq (symbol-function real)
+		      (get real 'definition-before-clcs))))
+    (setf (symbol-function real)
+	  (get real 'definition-before-clcs))))
+
+(defvar *clcs-redefinitions1*
+  (nconc (mapcar #'(lambda (symbol)
+		     (list (intern (symbol-name symbol) "LISP") symbol))
+		 *shadowed-symbols1*)
+	 '()))
+
+(defun install-clcs-symbols ()
+  (dolist (r *clcs-redefinitions1*)
+    (install-symbol (first r) (second r)))
+  nil)
+
+(defun revert-clcs-symbols ()
+  (dolist (r (reverse *clcs-redefinitions1*))
+    (revert-symbol (first r)))
+  nil)
+
+(install-clcs-symbols)
