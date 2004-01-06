e4d2a7a3021e1b7e6b0752d6265f83b41d5d44a8
Mike Thomas <mike.thomas@paradigmgeo.com>
2004-01-06 00:47:16 +0000

    Rationalise installer scripts.

--- gcl/windows/sysdir.bat
+++
@@ -1,5 +0,0 @@
-cd %1
-echo (setq si::*system-directory* (namestring(truename (make-pathname :name nil :type nil :defaults (si::argv 0))))) (si::save-system "modified.exe") | saved_gcl.exe 
-del saved_gcl.exe
-ren modified.exe saved_gcl.exe
-pause
