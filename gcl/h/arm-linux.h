0bca21074d1a850f4cf12f06d3fdd631a272a2f0
Camm Maguire <camm@debian.org>
2004-02-12 05:24:53 +0000

    SGC on for arm and hppa

--- gcl/h/arm-linux.h
+++ gcl/h/arm-linux.h
@@ -36,3 +36,14 @@
 		    : "=r" (_beg)				\
 		    : "0" (_beg), "r" (_end), "r"(_flg));	\
 } while (0)
+
+#ifdef IN_GBC
+#undef MPROTECT_ACTION_FLAGS
+#define MPROTECT_ACTION_FLAGS SA_RESTART|SA_SIGINFO
+#define GET_FAULT_ADDR(sig,code,sv,a) \
+ ((siginfo_t *)code)->si_addr
+/*  #define GET_FAULT_ADDR(sig,code,sv,a) \ */
+/*      ((void *)(*((char ***)(&code)))[44]) */
+#endif
+
+#define SGC
--- gcl/h/hppa-linux.h
+++ gcl/h/hppa-linux.h
@@ -17,3 +17,13 @@
 
 /*  #define CLEAR_CACHE do {void *v=memory->cfd.cfd_start,*ve=v+memory->cfd.cfd_size; for (;v<ve;v+=32)   asm __volatile__ ("dcbst 0,%0\n\tsync\n\ticbi 0,%0\n\tsync\n\tisync": : "r" (v) : "memory");} while(0) */
 
+#ifdef IN_GBC
+#undef MPROTECT_ACTION_FLAGS
+#define MPROTECT_ACTION_FLAGS SA_RESTART|SA_SIGINFO
+#define GET_FAULT_ADDR(sig,code,sv,a) \
+ ((siginfo_t *)code)->si_ptr
+/*  #define GET_FAULT_ADDR(sig,code,sv,a) \ */
+/*      ((void *)(*((char ***)(&code)))[44]) */
+#endif
+
+#define SGC
