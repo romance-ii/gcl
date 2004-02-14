03834031eba03d8357131a0a4f76be107b37e42d
Camm Maguire <camm@debian.org>
2004-02-14 17:36:30 +0000

    use standard sgc fault recovery element for hppa as recommended by hppa kernel experts

--- gcl/h/hppa-linux.h
+++ gcl/h/hppa-linux.h
@@ -21,7 +21,7 @@
 #undef MPROTECT_ACTION_FLAGS
 #define MPROTECT_ACTION_FLAGS SA_RESTART|SA_SIGINFO
 #define GET_FAULT_ADDR(sig,code,sv,a) \
- ((siginfo_t *)code)->si_ptr
+ ((siginfo_t *)code)->si_addr
 /*  #define GET_FAULT_ADDR(sig,code,sv,a) \ */
 /*      ((void *)(*((char ***)(&code)))[44]) */
 #endif
