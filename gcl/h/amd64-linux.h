b43eaba7987496c84578e9c56bd928f489baed26
Camm Maguire <camm@debian.org>
2004-06-11 03:42:55 +0000

    reenable SA_SIGINFO on amd64 to restore SGC there

--- gcl/h/amd64-linux.h
+++ gcl/h/amd64-linux.h
@@ -1,12 +1,10 @@
 #include "linux.h"
 
 #ifdef IN_GBC
-/*  #undef MPROTECT_ACTION_FLAGS */
-/*  #define MPROTECT_ACTION_FLAGS SA_RESTART|SA_SIGINFO */
-/*  #define GET_FAULT_ADDR(sig,code,sv,a) \ */
-/*   ((siginfo_t *)code)->si_addr */
+#undef MPROTECT_ACTION_FLAGS
+#define MPROTECT_ACTION_FLAGS SA_RESTART|SA_SIGINFO
 #define GET_FAULT_ADDR(sig,code,sv,a) \
-  ((siginfo_t *)code )->si_addr
+ ((siginfo_t *)code)->si_addr
 #endif
 
 /*#define NULL_OR_ON_C_STACK(x) ((x)==0 || ((unsigned int)x) > (unsigned int)(pagetochar(MAXPAGE+1)))*/
