631d6398ac1137dd6cd4e6b97aaa5e6fc0666033
Camm Maguire <camm@debian.org>
2004-08-05 22:35:07 +0000

    Build a profiling set of images as well for Debian, toggle between all four by default via debconf

--- gcl/debian/gcl.sh
+++ gcl/debian/gcl.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
-if [ "$GCL_ANSI" != "" ] ; then
-exec gcl/unixport/saved_ansi_gcl \
-   -dir gcl/unixport/ \
-   -libdir gcl/ \
-   -eval '(setq si::*allow-gzipped-file* t)' \
-   -eval '(setq si::*tk-library* "/usr/lib/tk8.3/../tk8.3")' \
-     "$@"
+
+EXT=@EXT@
+VERS=@VERS@
+
+. /etc/default/gcl$EXT
+if ! set | grep -q -w GCL_ANSI ; then GCL_ANSI=$DEFAULT_GCL_ANSI ; fi
+if ! set | grep -q -w GCL_PROF ; then GCL_PROF=$DEFAULT_GCL_PROF ; fi
+
+if [ "$GCL_PROF" = "" ] ; then 
+    DIR=/usr/lib/gcl-$VERS ; 
 else
-exec gcl/unixport/saved_gcl \
-   -dir gcl/unixport/ \
-   -libdir gcl/ \
-   -eval '(setq si::*allow-gzipped-file* t)' \
-   -eval '(setq si::*tk-library* "/usr/lib/tk8.3/../tk8.3")' \
-     "$@"
+    DIR=/usr/lib/gcl-$VERS-prof ; 
 fi
+
+if [ "$GCL_ANSI" = "" ] ; then 
+    EXE=saved_gcl;
+else
+    EXE=saved_ansi_gcl;
+fi
+SYS=$DIR/unixport
+
+exec $SYS/$EXE -dir $SYS/ -libdir $DIR/ \
+   -eval '(setq si::*allow-gzipped-file* t)' \
+   -eval '(setq si::*tk-library* "/usr/lib/tk@TKVERS@")' \
+     "$@"
+
 # other options: -load /tmp/foo.o -load jo.lsp -eval "(joe 3)"
