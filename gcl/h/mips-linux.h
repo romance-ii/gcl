94b33a3bcb3985311f1b402d6792c4f3c772aaa4
Camm Maguire <camm@debian.org>
2005-04-10 03:18:06 +0000

    cacheflush for mips; FIXME -- perhaps not necessary

--- gcl/h/mips-linux.h
+++ gcl/h/mips-linux.h
@@ -24,3 +24,12 @@
   ((siginfo_t *)code )->si_addr
 #endif
 #define SGC
+
+#include <asm/cachectl.h>
+int cacheflush(void *,int,int);
+#define CLEAR_CACHE_LINE_SIZE 32
+#define CLEAR_CACHE do {void *v=memory->cfd.cfd_start,*ve=v+memory->cfd.cfd_size; \
+                        v=(void *)((unsigned long)v & ~(CLEAR_CACHE_LINE_SIZE - 1));\
+                        cacheflush(v,ve-v,BCACHE);\
+                    } while(0)
+
