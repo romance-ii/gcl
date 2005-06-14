5a9fd05441bb72edc4f1ff8c021c71454b7ea27f
Camm Maguire <camm@debian.org>
2005-06-14 14:29:09 +0000

    Update NULL_OR_ON_C_STACK macro for alpha

--- gcl/h/alpha-linux.h
+++ gcl/h/alpha-linux.h
@@ -18,7 +18,8 @@
 /*  #define CLEAR_CACHE do {void *v=memory->cfd.cfd_start,*ve=v+memory->cfd.cfd_size; for (;v<ve;v+=32)   asm __volatile__ ("dcbst 0,%0\n\tsync\n\ticbi 0,%0\n\tsync\n\tisync": : "r" (v) : "memory");} while(0) */
 
 #define MUST_COPY_VA_LIST
-#define NULL_OR_ON_C_STACK(x) ((x)==0 || ((((unsigned long)x) > 0x100000000) && ((unsigned long)x) < 0x120000000))
+/* #define NULL_OR_ON_C_STACK(x) ((x)==0 || ((((unsigned long)x) > 0x100000000) && ((unsigned long)x) < 0x120000000)) */
+#define NULL_OR_ON_C_STACK(x) (((unsigned long)x) < DBEGIN || ((unsigned long)x) > (unsigned long)(pagetochar(MAXPAGE)))
 
 #undef MPROTECT_ACTION_FLAGS
 #define MPROTECT_ACTION_FLAGS SA_RESTART|SA_SIGINFO
