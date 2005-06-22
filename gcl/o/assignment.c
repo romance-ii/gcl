7cc7f11d368f8e29c5b4b4c228db28998fa71292
Camm Maguire <camm@debian.org>
2005-06-22 01:38:23 +0000

    Allow fmakunbound to proceed on undefined functions in cltl1 for acl2

--- gcl/o/assignment.c
+++ gcl/o/assignment.c
@@ -257,8 +257,11 @@ DEFUNO_NEW("FMAKUNBOUND",object,fLfmakunbound,LISP
   /* 1 args */
   if(type_of(sym) != t_symbol)
     not_a_symbol(sym);
+
+#ifdef ANSI_COMMON_LISP
   if (sym->s.s_gfdef==OBJNULL)
     FEundefined_function(sym);
+#endif
 
   if (sym->s.s_sfdef != NOT_SPECIAL) {
     if (sym->s.s_mflag) {
