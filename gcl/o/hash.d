f5910e9aa3f5598d487bd64f7e302b4dab9c69a5
Camm Maguire <camm@debian.org>
2005-08-10 13:59:56 +0000

    extra ansi hash functions

--- gcl/o/hash.d
+++ gcl/o/hash.d
@@ -688,6 +688,18 @@ DEFUN_NEW("HASH-TABLE-SIZE",object,fLhash_table_size,LISP,1,1,NONE,OO,OO,OO,OO,(
   RETURN1(make_fixnum(table->ht.ht_size));
 }
 
+DEFUN_NEW("HASH-TABLE-REHASH-SIZE",object,fLhash_table_rehash_size,LISP,1,1,NONE,OO,OO,OO,OO,(object table),"")
+{
+  check_type_hash_table(&table);
+  RETURN1(table->ht.ht_rhsize);
+}
+
+DEFUN_NEW("HASH-TABLE-REHASH-THRESHOLD",object,fLhash_table_rehash_threshold,LISP,1,1,NONE,OO,OO,OO,OO,(object table),"")
+{
+  check_type_hash_table(&table);
+  RETURN1(table->ht.ht_rhthresh);
+}
+
 
 
 void
