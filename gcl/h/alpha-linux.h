2b89455165bf0a8456b0fb168f992acffe7954a5
Camm Maguire <camm@debian.org>
2007-07-18 20:49:19 +0000

    local imb() for alpha

--- gcl/h/alpha-linux.h
+++ gcl/h/alpha-linux.h
@@ -8,7 +8,9 @@
   (char *)((struct ucontext *)scp )->uc_mcontext.sc_traparg_a0
 
 #define SGC
-#include <asm/system.h>
+#define PAL_imb		134
+#define imb() \
+__asm__ __volatile__ ("call_pal %0 #imb" : : "i" (PAL_imb) : "memory")
 #define CLEAR_CACHE imb()
 #define SPECIAL_BFD_INCLUDE "sfaslbfd_alpha.c"
 #define NOINTDIV
