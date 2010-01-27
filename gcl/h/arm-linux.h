7229b0299c5eac699f473fea19c17221bb3ac7dc
Camm Maguire <camm@debian.org>
2010-01-27 23:03:12 +0000

    __builtin___clear_cache on arm

--- gcl/h/arm-linux.h
+++ gcl/h/arm-linux.h
@@ -11,15 +11,19 @@
 /*		    : "a1");						\*/
 /*  } while (0) */
 
-#define CLEAR_CACHE do {\
-  void *v=memory->cfd.cfd_start,*ve=v+memory->cfd.cfd_size; \
-  register unsigned long _beg __asm ("a1") = (unsigned long)(v);	\
-  register unsigned long _end __asm ("a2") = (unsigned long)(ve);\
-  register unsigned long _flg __asm ("a3") = 0;			\
-  __asm __volatile ("swi 0x9f0002		@ sys_cacheflush"	\
-		    : "=r" (_beg)				\
-		    : "0" (_beg), "r" (_end), "r"(_flg));	\
-} while (0)
+/* #define CLEAR_CACHE do {\ */
+/*   void *v=memory->cfd.cfd_start,*ve=v+memory->cfd.cfd_size; \ */
+/*   register unsigned long _beg __asm ("a1") = (unsigned long)(v);	\ */
+/*   register unsigned long _end __asm ("a2") = (unsigned long)(ve);\ */
+/*   register unsigned long _flg __asm ("a3") = 0;			\ */
+/*   __asm __volatile ("swi 0x9f0002		@ sys_cacheflush"	\ */
+/* 		    : "=r" (_beg)				\ */
+/* 		    : "0" (_beg), "r" (_end), "r"(_flg));	\ */
+/* } while (0) */
+
+#define CLEAR_CACHE \
+  __builtin___clear_cache((void *)memory->cfd.cfd_start,\
+			  (void *)memory->cfd.cfd_start+memory->cfd.cfd_size)
 
 #define SGC
 #define NODYNEXT
