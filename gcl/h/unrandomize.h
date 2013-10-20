654891ff59e18eec7566cba1695b235719b71a4a
Camm Maguire <camm@debian.org>
2013-10-20 22:28:01 +0000

    robustify allocation of environment and argument space in unrandomize.h

--- gcl/h/unrandomize.h
+++ gcl/h/unrandomize.h
@@ -5,7 +5,6 @@
 #include <alloca.h>
 #include <errno.h>
 
-
 {
   errno=0;
 
@@ -20,20 +19,38 @@
     if ((pers & flag)!=flag && !getenv("GCL_UNRANDOMIZE")) {
       errno=0;
       if (personality(pers | flag) != -1 && (personality(0xffffffffUL) & flag)==flag) {
-	int i;
+	int i,j,k;
 	char **n,**a;
-	extern int *cs_org;
-	cs_org=(void *)&flag;/*get right stack for potential gc in malloc*/
-	for (i=0;envp[i];i++);
-	n=malloc((i+2)*sizeof(*n));
-	n[i+1]=0;
-	n[i--]="GCL_UNRANDOMIZE=t";
-	for (;i>=0;i--)
-	  n[i]=strdup(envp[i]);
+	void *v;
+	for (i=j=0;argv[i];i++)
+	  j+=strlen(argv[i])+1;
+	for (k=0;envp[k];k++)
+	  j+=strlen(envp[k])+1;
+	j+=(i+k+3)*sizeof(char *);
+	if ((v=sbrk(j))==(void *)-1) {
+	  printf("Cannot brk environment space\n");
+	  exit(-1);
+	}
+	a=v;
+	v=a+i+1;
+	n=v;
+	v=n+k+2;
+	for (i=0;argv[i];i++) {
+	  a[i]=v;
+	  strcpy(v,argv[i]);
+	  v+=strlen(v)+1;
+	}
+	a[i]=0;
+	for (k=0;envp[k];k++) {
+	  n[k]=v;
+	  strcpy(v,envp[k]);
+	  v+=strlen(v)+1;
+	}
+	n[k]="GCL_UNRANDOMIZE=t";
+	n[k+1]=0;
 #ifdef GCL_GPROF
 	gprof_cleanup();
 #endif
-	for (i=0;argv[i];i++);a=malloc((i+1)*sizeof(*argv));for (i=0;argv[i];i++) a[i]=strdup(argv[i]);a[i]=0;
 	errno=0;
 	execve(*a,a,n);
 	printf("execve failure %d\n",errno);
